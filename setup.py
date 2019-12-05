5111e811cb8781b13f7f8aff346d7f791b45016d
Zain-Saiyed <saiyedzain4@gmail.com>
2019-12-05 21:19:29 +0300

    Added .msi Windows Installer Package

--- setup.py
+++ setup.py
@@ -1,15 +1,38 @@
 from cx_Freeze import setup, Executable
+import sys
+
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 ## Two Player
+    
 setup(name='Two Player Chess game',
       version='1.0',
       description='Two Player Chess game',
-      executables=[Executable("Chess2player.py", base = "Win32GUI", icon='Chess_image/exe_icon.ico')]      
+      options = {
+          'build_exe':{
+              "include_files":["gameFonts/","Chess_image/"]
+            }
+          },
+      executables=[
+          Executable("Chess2player.py",
+                     base = base,
+                     icon='Chess_image/exe_icon.ico',
+                     shortcutName='Two Player Chess',
+                     targetName='Chess2P.exe')]      
 )
 
 ## AI
+    
 ##setup(name='AI Chess game',
 ##      version='1.0',
 ##      description='AI Chess game',
-##      executables=[Executable(script="AIChess.py", base = "Win32GUI", icon='Chess_image/exe_icon.ico')]      
+##      options = {
+##          'build_exe':{
+##              "include_files":["gameFonts/","Chess_image/"]
+##            }
+##          },
+##      executables=[Executable(script="AIChess.py", base = base, icon='Chess_image/exe_icon.ico')]      
 ##)
+
+    
